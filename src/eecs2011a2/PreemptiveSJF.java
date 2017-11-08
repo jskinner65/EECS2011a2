5304c926c7f179c1e2e937398239a1f01d82ec97
jason <jason@LAPTOP-L90IPECE>
2017-11-07 20:20:40 -0500

    Added some more code. Still working on it.

--- src/eecs2011a2/PreemptiveSJF.java
+++ src/eecs2011a2/PreemptiveSJF.java
@@ -1,16 +1,67 @@
 package eecs2011a2;
 
-public class PreemptiveSJF {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args){
-		
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreemptiveSJF implements Algorithms {
+	List<Processes> processes = new ArrayList<Processes>();
+	long totalWaitTime = 0;
+	int shortestJobIndex = 0;
+	int numberOfProcesses = 0;
+
+	public void runAlgorithm() {
+		long time = 0;
+		long processTime;
+		Test.createProcesses();
+		processes = Test.getProcesses();
+		while (!Test.allProcessDone()) {
+			processTime = 0;
+			System.out.println(shortestJobIndex);
+			System.out.println(processes.get(shortestJobIndex).getTotalTime());
+			System.out.println("Wait time was: " + waitTime(time));
+			System.out.println("StartTime: " + time);
+			while (processTime < processes.get(shortestJobIndex).getTotalTime()) {
+				processTime++;
+				time++;
+			}
+			System.out.println("EndTime: " + time);
+			System.out.println("");
+			processes.remove(shortestJobIndex);
+			if (!Test.allProcessDone()) {
+				shortestJob();
+			}
+		}
+		System.out.println("");
+		System.out.println("Average Wait Time was:  " + averageWait());
+	}
+
+	public void shortestJob() {
+		shortestJobIndex = 0;
+		int shortTime = processes.get(0).getTotalTime();
+		for (int i = 1; i < processes.size(); i++) {
+			if (processes.get(i).getTotalTime() < shortTime) {
+				shortTime = processes.get(i).getTotalTime();
+				shortestJobIndex = i;
+			}
+		}
+	}
+
+	@Override
+	public long waitTime(long time) {
+		long waitTime = (time - processes.get(shortestJobIndex).getQueuedTime());
+		processes.get(shortestJobIndex).setWaitTime(waitTime);
+		totalWaitTime += waitTime;
+		numberOfProcesses++;
+		return waitTime;
+	}
+
+	@Override
+	public long averageWait() {
+		return (long) (totalWaitTime / numberOfProcesses);
+	}
+
+	public static void main(String[] args) {
+		PreemptiveSJF start = new PreemptiveSJF();
+		start.runAlgorithm();
 	}
 }
